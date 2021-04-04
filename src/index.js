f4e12c8d90ecd8f4ce7be1c51ec76729a5b0f862
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2021-04-04 12:03:01 +0300

    section 3.communicating_with_props complited modified:   package-lock.json modified:   package.json modified:   public/index.html deleted:    src/App.css deleted:    src/App.js deleted:    src/App.test.js new file:   src/ApprovalCard.js new file:   src/CommentDetail.js deleted:    src/index.css modified:   src/index.js deleted:    src/logo.svg deleted:    src/reportWebVitals.js deleted:    src/setupTests.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const commentsData = [
+    { author: 'Sam', avatar: faker.image.avatar(), content: 'Nice blog post', timeAgo: 'Yesterday at 4:32 PM' },
+    { author: 'Mike', avatar: faker.image.avatar(), content: 'i like the subject', timeAgo: 'Yesterday at 6:10 PM' },
+    { author: 'Manu', avatar: faker.image.avatar(), content: 'I like the writing', timeAgo: 'Today at 10:03 AM' },
+    { author: 'Hey', avatar: faker.image.avatar(), content: 'It is good', timeAgo: 'Today at 3:52 PM' }
+  ]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const commentsList = commentsData.map(elem => {
+    return <ApprovalCard key={elem.author}><CommentDetail author={elem.author} avatar={elem.avatar} content={elem.content} timeAgo={elem.timeAgo} /></ApprovalCard>
+  });
+
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          <div>Are you sure you want to do this?</div>
+        </div>
+      </ApprovalCard>
+      {commentsList}
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
